37aee8ba7eeb1426ef2e1b0a6bf4ce3ff447018e
urooyo <inertia_mailer_0c@icloud.com>
2025-05-18 03:48:31 +0900

    feat: 디아로그를 컴포넌트화하고 임시로 추가함.

---
+++ src/component/dialog.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Typography from '@mui/material/Typography';
+
+export default function AlertDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="text" onClick={handleClickOpen}>
+        <Typography variant="body1" color="var(--foreground)">작은 기록장</Typography>
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"잠시만요!"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            이 페이지는 API를 사용해요. 최대 요청 수가 있으니 여러번 들어가시거나 등 나쁜 짓은 하지 말아주세요, 부탁합니다!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant='text' onClick={handleClose} autoFocus>취소</Button>
+          <Button href="https://moffu.me/blog">
+            이동하기
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
