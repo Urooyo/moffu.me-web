37f92aed9f4d651f1ec44ac9b089e0b355931e47
urooyo <inertia_mailer_0c@icloud.com>
2025-05-22 22:54:50 +0900

    feat: fetch 보낼 tsx로 대체

--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -1,35 +1,10 @@
-export const dynamicParams = true;
-export const dynamic = 'force-dynamic'
+// src/app/memo/page.tsx (Server Component)
 
-import { Any } from 'next-sanity';
-import { PortableText } from 'next-sanity';
-import { getPosts } from '@/lib/sanity.client'
-import { Box, Container, Typography } from "@mui/material";
+import { getPosts } from '@/lib/sanity.client';
+import BlogClient from '@/app/memo/BlogClient';
 
 export default async function BlogPage() {
-  const posts = await getPosts()
+  const posts = await getPosts();
 
-  return (
-    <Container>
-      <Container>
-        <Typography variant='h2'>나의 작은 기록장</Typography>
-        <Box>
-          {posts.map((post: any, index: number) => (
-            <Box key={`${post._id}-${index}`} mb={4}>
-              <Typography variant="h4" gutterBottom>
-                {post.title}
-              </Typography>
-              <Typography variant="body1">
-                <PortableText value={post.body} />
-              </Typography>
-              <Typography variant="caption" color="textSecondary">
-                {new Date(post.publishedAt).toLocaleDateString()}
-              </Typography>
-            </Box>
-          ))}
-        </Box>
-      </Container>
-    </Container>
-  )}
-
-  // <PortableText value={post.body} />
+  return <BlogClient posts={posts} />;
+}
